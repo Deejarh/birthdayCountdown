f39fb32b774b1faa00fe8e810fd8d462edd79f5c
Badmus Damola Khadijah <Damolabadmus014@gmail.com>
2024-01-08 22:12:49 +0000

    text update

--- src/components/CountdownHeader.vue
+++ src/components/CountdownHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="text-center">
     <h2 class="text-2xl text-green"></h2>
-    <h1 class="mt-2 mb-5 text-4xl">Birthday Countdown</h1>
+    <h1 class="mt-2 mb-5 lg:text-4xl text-xl ">Birthday Countdown</h1>
   </header>
 </template>
